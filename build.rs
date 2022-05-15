3868e9a3cc844b7fb13ae68cf9647828b925c7e2
bdbai <bdbaiapp@163.com>
2022-05-16 00:43:49 +0800

    add uwp support

--- build.rs
+++ build.rs
@@ -117,7 +117,8 @@ fn generate_lwip_bindings() {
 
 fn main() {
     let os = env::var("CARGO_CFG_TARGET_OS").unwrap();
-    if os == "ios" || os == "android" || os == "linux" || os == "macos" {
+    let vendor = env::var("CARGO_CFG_TARGET_VENDOR").unwrap();
+    if os == "ios" || os == "android" || os == "linux" || os == "macos" || vendor == "uwp" {
         compile_lwip();
     }
 
